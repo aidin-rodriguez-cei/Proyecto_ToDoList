4b9a7cb6852ff08650d77fa9a56584997e52767c
AidinR <rodriguezaidin@alumnos.cei.es>
2025-01-08 23:02:28 +0100

    segunda parte

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 const Login = () => {
+    const [formData, setFormData] = useState({
+        username:"",
+        password:"",
+        // Info del backend 
+        name:  "Aidin",
+        image: "https://picsum.photos/200"
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name] : value
+        });
+
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
     return (  
-        <>
+        <form onSubmit={handleSubmit}
+        className="flex flex-col gap-4"
+        >
         <h1>Soy Login</h1>
-        <p>Formulario</p>
-        </>
+        
+        <input onChange={handleChange} value={formData.username} type="email" name="username" placeholder="Usuario" requeried/>
+        <input onChange={handleChange} value={formData.password} type="password" name="password" placeholder="Clave" requeried/>
+        <input onChange={handleChange} type="submit" name="Login" />
+
+        </form>
     );
 }
 export default Login;
--- Frontend/src/pages/Registro.jsx
+++ Frontend/src/pages/Registro.jsx
@@ -1,9 +1,82 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Registro = () => {
-    return (  
-        <>
-        <h1>Soy Registro</h1>
-        <p>Formulario</p>
-        </>
+    const [canSubmit, setCanSubmit] = useState(false);
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        name: "",
+        tyc: false,
+        image: "https://picsum.photos/200"
+    });
+
+    // Botón de Submit
+    useEffect(() => {
+        setCanSubmit(formData.name && formData.username && formData.password && formData.tyc);
+    }, [formData]);
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormData({
+            ...formData,
+            [name]: type === "checkbox" ? checked : value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(formData);
+        navigate("/admin");
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <h1>Registro</h1>
+
+            <input 
+                onChange={handleChange} 
+                value={formData.name}  
+                type="text" 
+                name="name" 
+                placeholder="Nombre" 
+                required
+            />
+
+            <input 
+                onChange={handleChange} 
+                value={formData.username} 
+                type="email" 
+                name="username" 
+                placeholder="Usuario" 
+                required
+            />
+
+            <input 
+                onChange={handleChange} 
+                value={formData.password} 
+                type="password" 
+                name="password" 
+                placeholder="Clave" 
+                required
+            />
+
+            <label>
+                <input 
+                    onChange={handleChange} 
+                    name="tyc" 
+                    type="checkbox" 
+                /> Acepto TyC
+            </label>
+
+            {canSubmit ? 
+                <button type="submit">Registro</button> : 
+                <button disabled>Complete todos los datos</button>
+            }
+        </form>
     );
-}
-export default Registro;Registro
+};
+
+export default Registro;
